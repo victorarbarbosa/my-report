135d429af42303576f1ed2eed48e9818671dfc07
Victor Barbosa <victorabarbosa03@gmail.com>
2025-10-08 22:14:58 -0300

    feat: adicionando estrutura inicial da api.

---
+++ my-report/src/main/java/com/myreport/api/MyReportApplication.java
@@ -0,0 +1,13 @@
+package com.myreport.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyReportApplication.class, args);
+	}
+
+}
