5077cfeb9152e1f74a0016478bbdf7b6fd2e5a28
Victor Barbosa <victorabarbosa03@gmail.com>
2025-10-30 20:50:09 -0300

    feat: adicionando projeto frontend

---
+++ my-report-frontend/my-report-frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+
+  return (
+    <main>
+      <div className='container signin-container'>
+        <div className='col-sm-6 col-md-4 col-lg-3'>
+          <h1>Hello World!</h1>
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default App
